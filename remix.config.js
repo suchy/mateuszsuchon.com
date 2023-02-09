db61f247a6b0344ea139e5081541b41d06435d8e
Mateusz Suchoń <kontakt@suchego.pl>
2023-02-09 19:51:02 +0100

    Add prism for syntax highlighting

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,13 @@
 /** @type {import('@remix-run/dev').AppConfig} */
+
+const rehypePrism = require("@mapbox/rehype-prism");
+
 module.exports = {
   serverBuildTarget: "cloudflare-workers",
   server: "./server.js",
   devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
+  mdx: {
+    rehypePlugins: [rehypePrism],
+  },
 };
